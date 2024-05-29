367e046c03597e8a6f064bdf191ed7dcb0f8f484
Jan Steiner <jansteiner73@gmail.com>
2024-05-29 13:44:16 +0200

    adjusted filter-function

--- script.js
+++ script.js
@@ -60,15 +60,16 @@ function filterPokemon() {
     let pokemonSection = document.getElementById('pokemon-section');
     pokemonSection.innerHTML = '';
 
-    for (let i = 0; i < pokemon.results.length; i++) {
-        let name = pokemon.results[i].name;
+    for (let i = 0; i < pokemonNames.results.length; i++) {
+        let name = pokemonNames.results[i].name;
+        let type = pokemonData[i].types[0].type.name;
         if (name.toLowerCase().includes(search)) {
             pokemonSection.innerHTML += `
             <div id="pokemon-box">
                 <h3>${name}</h3>
                 <div>#${i + 1}</div>
                 <img id="pokemon-box-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png"
-                <div>element</div>
+                <div>${type}</div>
             </div>
         `
         }
