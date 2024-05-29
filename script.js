740c07414371b0831eb544268d6e1399e1f3c3ca
Jan Steiner <jansteiner73@gmail.com>
2024-05-29 13:37:56 +0200

    fetch-function for complete pokemon data

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 let pokemonNames = [];
-let pokemonInfos = [];
+let pokemonData = [];
 
 const BASE_URL = "https://pokeapi.co/api/v2/"
 
 
-function init(){
+function init() {
     fetchPokemonNames();
 }
 
 async function fetchPokemonNames() {
-    let response = await fetch(BASE_URL + `pokemon?limit=10&offset=0`);
+    let response = await fetch(BASE_URL + `pokemon?limit=30&offset=0`);
     let responseAsJson = await response.json()
     pokemonNames = responseAsJson;
     console.log(pokemonNames);
+    fetchPokemonData();
+}
+
+async function fetchPokemonData() {
+    for (let i = 0; i < pokemonNames.results.length; i++) {
+        let response = await fetch(BASE_URL + `pokemon/${i+1}`);
+        let responseAsJson = await response.json();
+        pokemonData.push(responseAsJson);
+    }
+    console.log(pokemonData)
+
     showPokemon();
 }
 
@@ -22,13 +33,13 @@ function showPokemon() {
 
     for (let i = 0; i < pokemonNames.results.length; i++) {
         let name = pokemonNames.results[i].name;
-        let element = '';
+        let type = pokemonData[i].types[0].type.name;
         pokemonSection.innerHTML += `
             <div id="pokemon-box">
                 <h3>${name}</h3>
                 <div>#${i + 1}</div>
                 <img id="pokemon-box-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png"
-                <div>element</div>
+                <div>${type}</div>
             </div>
         `
     }
