e3dce48095d10beea4ee438847aa372f952607ad
Jan Steiner <jansteiner73@gmail.com>
2024-05-29 17:03:57 +0200

    modified fetch, render & pokemon-box-css

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-let pokemonNames = [];
 let pokemonData = [];
+let index = 0
 
 const BASE_URL = "https://pokeapi.co/api/v2/"
 
 
 function init() {
-    fetchPokemonNames();
-}
-
-async function fetchPokemonNames() {
-    let response = await fetch(BASE_URL + `pokemon?limit=30&offset=0`);
-    let responseAsJson = await response.json()
-    pokemonNames = responseAsJson;
-    console.log(pokemonNames);
     fetchPokemonData();
 }
 
 async function fetchPokemonData() {
-    for (let i = 0; i < pokemonNames.results.length; i++) {
+    for (let i = index; i < index +30; i++) {
         let response = await fetch(BASE_URL + `pokemon/${i+1}`);
         let responseAsJson = await response.json();
         pokemonData.push(responseAsJson);
     }
     console.log(pokemonData)
-
     showPokemon();
+    index += 30;
 }
 
 function showPokemon() {
     let pokemonSection = document.getElementById('pokemon-section');
     pokemonSection.innerHTML = '';
 
-    for (let i = 0; i < pokemonNames.results.length; i++) {
-        let name = pokemonNames.results[i].name;
+    for (let i = 0; i < pokemonData.length; i++) {
+        let name = pokemonData[i].name;
         let type = pokemonData[i].types[0].type.name;
         pokemonSection.innerHTML += `
-            <div id="pokemon-box">
-                <h3>${name}</h3>
-                <div>#${i + 1}</div>
-                <img id="pokemon-box-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png"
-                <div>${type}</div>
+            <div class="bg-${type}" id="pokemon-box">
+                <div class="row-sb">
+                    <h3>${name}</h3>
+                    <div>#${i + 1}</div>
+                </div>
+                <img id="pokemon-box-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i + 1}.png">
+                <div class="type-field-${type}">${type}</div>
             </div>
         `
     }
@@ -60,8 +54,8 @@ function filterPokemon() {
     let pokemonSection = document.getElementById('pokemon-section');
     pokemonSection.innerHTML = '';
 
-    for (let i = 0; i < pokemonNames.results.length; i++) {
-        let name = pokemonNames.results[i].name;
+    for (let i = 0; i < pokemonData.length; i++) {
+        let name = pokemonData[i].name;
         let type = pokemonData[i].types[0].type.name;
         if (name.toLowerCase().includes(search)) {
             pokemonSection.innerHTML += `
